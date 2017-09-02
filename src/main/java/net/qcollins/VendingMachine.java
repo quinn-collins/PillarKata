a01d7cfee5a1bcbeee3960e3727dd502216ffafb
Quinn Collins <collinsqui@gmail.com>
2017-09-02 18:30:32 -0400

    setup VendingMachineTest so that I may start writing tests

--- src/main/java/net/qcollins/VendingMachine.java
+++ src/main/java/net/qcollins/VendingMachine.java
@@ -2,4 +2,8 @@ package net.qcollins;
 
 public class VendingMachine {
 
+	public VendingMachine(CoinIdentifier coinIdentifier) {
+		// TODO Auto-generated constructor stub
+	}
+
 }
--- src/test/java/net/qcollins/VendingMachineTest.java
+++ src/test/java/net/qcollins/VendingMachineTest.java
@@ -1,5 +1,15 @@
 package net.qcollins;
 
+import org.junit.Before;
+
 public class VendingMachineTest {
+	VendingMachine vendingMachine;
+	CoinIdentifier coinIdentifier;
 	
+	@Before
+	public void setup() {
+		coinIdentifier = new CoinIdentifier();
+		vendingMachine = new VendingMachine(coinIdentifier);
+	}
 }
+
