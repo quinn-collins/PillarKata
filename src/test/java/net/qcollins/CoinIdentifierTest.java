870ef0afb0f9e6596ecbb9361d5f1d0ec8afdec7
Quinn Collins <collinsqui@gmail.com>
2017-09-04 18:27:04 -0400

    I finished the requirements of the Kata and am now emailing my solution.

--- src/test/java/net/qcollins/CoinIdentifierTest.java
+++ src/test/java/net/qcollins/CoinIdentifierTest.java
@@ -14,46 +14,46 @@ public class CoinIdentifierTest {
 	
 	@Test
 	public void identifierCorrectlyIdentifiesQuarter() {
-		Assert.assertEquals("quarter", coinIdentifier.identify(5.670, 24.26, 1.75));
+		Assert.assertEquals("quarter", coinIdentifier.identify(5.670, 24.26, 1.75).getName());
 	}
 	
 	@Test
 	public void identifierCorrectlyIdentifiesDime() {
-		Assert.assertEquals("dime", coinIdentifier.identify(2.268, 17.91, 1.35));
+		Assert.assertEquals("dime", coinIdentifier.identify(2.268, 17.91, 1.35).getName());
 	}
 	
 	@Test
 	public void identifierCorrectlyIdentifiesNickel() {
-		Assert.assertEquals("nickel", coinIdentifier.identify(5, 21.21, 1.95));
+		Assert.assertEquals("nickel", coinIdentifier.identify(5, 21.21, 1.95).getName());
 	}
 	
 	@Test
 	public void identifierReturnsRejectedIfCriteriaNotMet() {
-		Assert.assertEquals("rejected", coinIdentifier.identify(2.5, 19, 1.52));
+		Assert.assertEquals("rejected", coinIdentifier.identify(2.5, 19, 1.52).getName());
 	}
 	
 	@Test
 	public void identifierIdentifiesQuartersThatHaveBeenWorn() {
-		Assert.assertEquals("quarter", coinIdentifier.identify(5.570, 24.26, 1.75));
+		Assert.assertEquals("quarter", coinIdentifier.identify(5.570, 24.26, 1.75).getName());
 	}
 	
 	@Test
 	public void identifierIdentifiesQuartersThatHasExtraWeight() {
-		Assert.assertEquals("quarter", coinIdentifier.identify(5.770, 24.26, 1.75));
+		Assert.assertEquals("quarter", coinIdentifier.identify(5.770, 24.26, 1.75).getName());
 	}
 	
 	@Test
 	public void identifierIdentifiesDimesThatHaveBeenWorn() {
-		Assert.assertEquals("dime", coinIdentifier.identify(2.168, 17.91, 1.35));
+		Assert.assertEquals("dime", coinIdentifier.identify(2.168, 17.91, 1.35).getName());
 	}
 	
 	@Test
 	public void identifierIdentifiesDimesThatHasExtraWeight() {
-		Assert.assertEquals("dime", coinIdentifier.identify(2.368, 17.91, 1.35));
+		Assert.assertEquals("dime", coinIdentifier.identify(2.368, 17.91, 1.35).getName());
 	}
 	
 	@Test
 	public void identifierIdentifiesNickelsThatHaveBeenWorn() {
-		Assert.assertEquals("nickel", coinIdentifier.identify(5.1, 21.21, 1.95));
+		Assert.assertEquals("nickel", coinIdentifier.identify(5.1, 21.21, 1.95).getName());
 	}
 }
